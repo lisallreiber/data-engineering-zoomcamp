0ca7dc9859c3bad0a9b1c9ab5fba81865a78b763
lisallreiber <lisallreiber@gmail.com>
2023-01-18 13:03:19 +0100

    refactor: add new way to pass env variables

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -1,12 +1,12 @@
 import sys
-
+import os 
 import pandas as pd
 
 # for debugging print all the args in the system environment
 # print(sys.argv)
 
-day = sys.argv[1]
-
+# day = sys.argv[1]
+day = os.getenv("DAY")
 # some fancy stuff with pandas
 
 print(f'job finished successfully for day = {day}')
