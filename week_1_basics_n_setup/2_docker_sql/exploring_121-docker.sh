0e070265f35506ff05e7b99275b1b4af45edc917
lisallreiber <lisallreiber@gmail.com>
2023-01-19 12:30:16 +0100

    refactor: update from markdown to shell script

--- week_1_basics_n_setup/2_docker_sql/exploring_121-docker.sh
+++ week_1_basics_n_setup/2_docker_sql/exploring_121-docker.sh
@@ -9,8 +9,7 @@ docker run -it ubuntu bash
 # run an image with specified tag (3.9) and open python 
 docker run -it python:3.9
 
-# problem: in the previous image, none of the libraries like pandas or os are available
-# therefore we want to install pandas on the image, before we enter the python environment
+# problem: in the previous image, none of the libraries like pandas or os are available. Therefore we want to install pandas on the image, before we enter the python environment
 # solution: we overwrite the python entrypoint and manually install pandas in the interactive mode
 
 docker run -it --entrypoint=bash python:3.9
@@ -19,16 +18,18 @@ python                                              # opens python in the intera
 
 # problem: when we run the image again, we also need to install pandas again
 # solution: build your own customized image with docker build and dockerfiles
+
 docker build -t test:pandas .                       # image is build based on the dockerfile in the current directory
 docker run -it test:pandas                          # with v0 code uncommented in dockerfile
 
 
 # problem: we don't want to run our oython scripts manually in the container
 # solution: now we create a python script (pipeline) which we run in the container
-docker build -t test:python_pipeline .              # image is build based on the dockerfile in the current directory
-docker run -it test:python_pipeline 2022-01-17      # with v1.1 code uncommented in dockerfile and day parameter
+
+docker build -t test:python_pipeline .                      # image is build based on the dockerfile in the current directory
+docker run -it test:python_pipeline 2022-01-17              # with v1.1 code uncommented in dockerfile and day parameter
 docker run -it -e "DAY=2023-01-17" test:python_pipeline     # with v1.1 code uncommented in dockerfile and day parameter
 
 # problem: how to load data
 docker build -t pandas:test_pipeline .              # image is build based on the dockerfile in the current directory
-docker run pandas:test_pipeline .                   # with v1.2 code uncommented in dockerfile
+docker run pandas:test_pipeline .                   # with v1.2 code uncommented in Dockerfile
