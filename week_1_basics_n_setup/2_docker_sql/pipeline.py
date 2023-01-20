b73593f057ccb9b35b62034970770ca99b179460
lisallreiber <lisallreiber@gmail.com>
2023-01-20 15:53:56 +0100

    add: add data ingestion pipeline

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -9,4 +9,15 @@ import pandas as pd
 day = os.getenv("DAY")
 # some fancy stuff with pandas
 
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python upload-data.py \
+    --user = root \
+    --password = root \
+    --host = localhost \
+    --port = 5432 \
+    --db_name = ny_taxi \
+    --tbl_name = yellow_taxi_data \
+    --url = ${URL}
+
 print(f'job finished successfully for day = {day}')
