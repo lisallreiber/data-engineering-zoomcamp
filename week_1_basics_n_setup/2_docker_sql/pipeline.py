961d60802ee7dc05392a85e0ebfce87ca5a65f6b
lisallreiber <lisallreiber@gmail.com>
2023-01-23 10:05:23 +0100

    refactor: move shell commandy to explore script

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -1,23 +1,13 @@
-import sys
+
 import os 
 import pandas as pd
 
 # for debugging print all the args in the system environment
+# import sys
 # print(sys.argv)
-
 # day = sys.argv[1]
+
 day = os.getenv("DAY")
 # some fancy stuff with pandas
-
-URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-
-python upload-data.py \
-    --user = root \
-    --password = root \
-    --host = localhost \
-    --port = 5432 \
-    --db_name = ny_taxi \
-    --tbl_name = yellow_taxi_data \
-    --url = ${URL}
 
 print(f'job finished successfully for day = {day}')
